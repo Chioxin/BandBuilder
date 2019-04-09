86b9b98c7cd4a3e49935208351faf736c44d8681
Todd Trowbridge <todd.m.trowbridge@gmail.com>
2019-04-09 16:27:06 -0600

    stub jpa and boot projects, link project dependencies

---
+++ BandBuilder/src/main/java/com/skilldistillery/bandbuilder/BandBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.bandbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandBuilderApplication.class, args);
+	}
+
+}
