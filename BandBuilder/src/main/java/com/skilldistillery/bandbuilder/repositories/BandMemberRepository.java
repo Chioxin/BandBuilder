354e05384cde91b6436908f4d0c914d70d0a4434
Todd Trowbridge <todd.m.trowbridge@gmail.com>
2019-04-12 15:13:08 -0600

    push for merge

--- BandBuilder/src/main/java/com/skilldistillery/bandbuilder/repositories/BandMemberRepository.java
+++ BandBuilder/src/main/java/com/skilldistillery/bandbuilder/repositories/BandMemberRepository.java
@@ -1,9 +1,13 @@
 package com.skilldistillery.bandbuilder.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.bandbuilder.entities.BandMember;
 
 public interface BandMemberRepository extends JpaRepository<BandMember, Integer> {
 
+	public List<BandMember> getBandMembersByBandId(int id);
+	public BandMember getBandMemberById(int id);
 }
---
+++ BandBuilder/src/main/java/com/skilldistillery/bandbuilder/services/BandMemberService.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.bandbuilder.services;
+
+import java.util.List;
+
+import com.skilldistillery.bandbuilder.datatransferobjects.BandMemberDTO;
+import com.skilldistillery.bandbuilder.entities.BandMember;
+
+public interface BandMemberService {
+
+	public List<BandMember> getBandMembersByBandId(int id);
+	public BandMember getBandMemberById(int id);
+	public BandMember createBandMember(BandMemberDTO bandMemberDTO);
+	public BandMember updateBandMember(int id, BandMember bandMember);
+	public Boolean deleteBandMember(int id);
+	
+}
