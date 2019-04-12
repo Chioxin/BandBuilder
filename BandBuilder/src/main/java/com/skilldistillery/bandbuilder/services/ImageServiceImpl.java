8b40330b20827c8163fc0f3461635332a0d1d626
Chioxin <chioxin@gmail.com>
2019-04-12 10:52:05 -0600

    created image service classes

---
+++ BandBuilder/src/main/java/com/skilldistillery/bandbuilder/services/ImageServiceImpl.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.bandbuilder.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.skilldistillery.bandbuilder.entities.Image;
+import com.skilldistillery.bandbuilder.repositories.ImageRepository;
+
+public class ImageServiceImpl implements ImageService {
+	
+	@Autowired
+	private ImageRepository imageRepo;
+
+	@Override
+	public Image getImageById(int id) {
+		Image image = null;
+		Optional<Image> opt = imageRepo.findById(id);
+		
+		if (opt.isPresent()) {
+			image = opt.get();
+		}
+		
+		return image;
+	}
+
+	@Override
+	public Image createImage(Image image) {
+		return imageRepo.saveAndFlush(image);
+	}
+
+	@Override
+	public Image updateImage(int id, Image image) {
+		Image managed = null;
+		Optional<Image> opt = imageRepo.findById(id);
+		
+		if (opt.isPresent()) {
+			managed = opt.get();
+			managed.setDescription(image.getDescription());
+			managed.setUrl(image.getUrl());
+		}
+		return null;
+	}
+
+	@Override
+	public Boolean deleteImage(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
