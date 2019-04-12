27fda67764a40df8d01fa7caab38ec5f9381bf35
Chioxin <chioxin@gmail.com>
2019-04-11 19:13:47 -0600

    completed profile controller and services mostly

--- BandBuilder/src/main/java/com/skilldistillery/bandbuilder/controllers/ProfileController.java
+++ BandBuilder/src/main/java/com/skilldistillery/bandbuilder/controllers/ProfileController.java
@@ -6,9 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,9 +49,45 @@ public class ProfileController {
 	public Profile createProfile(@RequestBody RegistrationDTO regInfo,
 			HttpServletResponse resp,
 			HttpServletRequest req) {
+		
 		Profile profile = profileSvc.createProfile(regInfo);
 		
+		if (profile != null) {
+			resp.setStatus(201);
+			StringBuffer url = req.getRequestURL();
+			url.append("/");
+			url.append(profile.getId());
+			resp.setHeader("Location", url.toString());
+		} else {
+			resp.setStatus(400);
+		}
 		
 		return profile;
+	}
+	
+	@PutMapping("profiles/{pid}")
+	public Profile updateProfile(@RequestBody Profile updatedProfile, @PathVariable("pid") Integer id,
+			HttpServletResponse resp) {
+		
+		profileSvc.updateProfileById(id, updatedProfile);
+		
+		if (updatedProfile != null) {
+			resp.setStatus(201);
+		} else {
+			resp.setStatus(400);
+		}
+		
+		return updatedProfile;
+	}
+	
+	@DeleteMapping("profiles/{pid}")
+	public void deleteProfile(@PathVariable("pid") Integer id,
+			HttpServletResponse resp) {
+		
+		if (profileSvc.deleteProfileById(id)) {
+			resp.setStatus(204);
+		} else {
+			resp.setStatus(400);
+		}		
 	}
 }
