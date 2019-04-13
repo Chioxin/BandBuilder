e9ec5befb9692bf1ced90bb3c1b000a34697745e
Jesse <Jesse.mena4@gmail.com>
2019-04-13 12:01:42 -0600

    Feat/Update: create new home and nav-bar components in angualar, updated all controller to use angular port 4220

--- ngBandBuilder/src/app/app.module.ts
+++ ngBandBuilder/src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BandBuilderComponent } from './components/band-builder/band-builder.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BandBuilderComponent
+    BandBuilderComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
