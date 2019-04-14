097024f6843f9dcd1c94b50026db53bc00f582c1
Jesse <Jesse.mena4@gmail.com>
2019-04-14 17:48:20 -0600

    Update: updated the band_member.service

--- ngBandBuilder/src/app/services/band-member.service.ts
+++ ngBandBuilder/src/app/services/band-member.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { BandMember } from '../models/band-member';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BandMemberService {
+  // FIELDS
+  private apiRoute = 'api/bandmembers/';
+  private baseUrl = environment.baseUrl;
+  private url = this.baseUrl + this.apiRoute;
 
-  constructor() { }
+  // CONSTRUCTOR
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService
+  ) { }
+  showByBandId(id: number) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Basic ' + this.auth.getCredentials()
+      })
+    };
+
+    return this.http.get<BandMember[]>(`/api/bands/${id}/bandmembers/`, httpOptions);
+  }
+  create(bandMember: BandMember) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Basic ' + this.auth.getCredentials()
+      })
+    };
+    return this.http.post<BandMember>(this.url, bandMember, httpOptions);
+  }
+  update(id: number, bandMember: BandMember) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Basic ' + this.auth.getCredentials()
+      })
+    };
+
+    return this.http.put<BandMember[]>(this.url + id, bandMember, httpOptions);
+  }
+  delete(id: number) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Basic ' + this.auth.getCredentials()
+      })
+    };
+    return this.http.delete<BandMember>(this.url + id, httpOptions);
+  }
 }
