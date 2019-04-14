ab3f31e50e81ffddcc6d16991fdc51b256758b13
Chioxin <chioxin@gmail.com>
2019-04-14 16:44:17 -0600

    created address service

--- ngBandBuilder/src/app/services/address.service.ts
+++ ngBandBuilder/src/app/services/address.service.ts
@@ -1,3 +1,7 @@
+import { Address } from './../models/address';
+import { environment } from 'src/environments/environment';
+import { AuthService } from './auth.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +9,36 @@ import { Injectable } from '@angular/core';
 })
 export class AddressService {
 
-  constructor() { }
+  // FIELDS
+
+  private apiRoute = 'api/addresses/';
+  private baseUrl = environment.baseUrl;
+  private url = this.baseUrl + this.apiRoute;
+
+  // CONSTRUCTOR
+
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService
+    ) { }
+
+  // METHODS
+
+  show(id: number) {
+    const httpOptions = {headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + this.auth.getCredentials()
+    })};
+
+    return this.http.get<Address>(this.url + id, httpOptions);
+  }
+
+  udpate(id: number, address: Address) {
+    const httpOptions = {headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + this.auth.getCredentials()
+    })};
+
+    return this.http.put<Address>(this.url + id, address, httpOptions);
+  }
 }
