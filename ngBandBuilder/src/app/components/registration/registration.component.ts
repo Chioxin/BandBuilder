736e0c8a445f83c189e22d3391fde170f8c73183
Chioxin <chioxin@gmail.com>
2019-04-14 14:00:13 -0600

    can now create a user and profile

--- ngBandBuilder/src/app/components/registration/registration.component.ts
+++ ngBandBuilder/src/app/components/registration/registration.component.ts
@@ -1,5 +1,8 @@
+import { AuthService } from 'src/app/services/auth.service';
 import { RegistrationForm } from './../../models/registration-form';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-registration',
@@ -14,7 +17,10 @@ export class RegistrationComponent implements OnInit {
 
   // CONSTRUCTOR
 
-  constructor() { }
+  constructor(
+    private profileSvc: ProfileService,
+    private auth: AuthService
+  ) { }
 
   // INIT
 
@@ -22,5 +28,45 @@ export class RegistrationComponent implements OnInit {
   }
 
   // METHODS
+
+  registerUser() {
+    const newUser = new User();
+    newUser.username = this.regInfo.userUsername;
+    newUser.password = this.regInfo.userPassword;
+
+    this.auth.register(newUser).subscribe(
+      dataRegister => {
+        this.regInfo.userId = dataRegister.id;
+        this.auth.login(this.regInfo.userUsername, this.regInfo.userPassword).subscribe(
+          dataLogin => {
+            this.createProfile();
+          },
+          err => {
+            console.error('ERROR LOGGING IN');
+            console.error(err);
+          }
+        );
+      },
+      err => {
+        console.error('ERROR REGISTERING USER');
+        console.error(err);
+      }
+    );
+
+  }
+
+  createProfile() {
+    console.log(this.regInfo);
+    this.profileSvc.create(this.regInfo).subscribe(
+      dataProfile => {
+        console.log('WE GOT HERE YAY');
+        // ROUTE USER TO PROFILE PAGE HERE
+      },
+      err => {
+        console.error('ERROR CREATING PROFILE');
+        console.error(err);
+      }
+    );
+  }
 
 }
--- ngBandBuilder/src/app/services/profile.service.ts
+++ ngBandBuilder/src/app/services/profile.service.ts
@@ -1,3 +1,8 @@
+import { RegistrationForm } from './../models/registration-form';
+import { Profile } from './../models/profile';
+import { AuthService } from './auth.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,10 +12,42 @@ export class ProfileService {
 
   // FIELDS
 
+  private apiRoute = 'api/profiles/';
+  private baseUrl = environment.baseUrl;
+  private url = this.baseUrl + this.apiRoute;
+
   // CONSTRUCTOR
 
-  constructor() { }
+  constructor(private http: HttpClient,
+              private auth: AuthService) { }
 
   // METHODS
+
+  index() {
+    const httpOptions = {headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + this.auth.getCredentials()
+    })};
+
+    return this.http.get<Profile[]>(this.url, httpOptions);
+  }
+
+  show(id: number) {
+    const httpOptions = {headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + this.auth.getCredentials()
+    })};
+
+    return this.http.get<Profile>(this.url + id, httpOptions);
+
+  }
+
+  create(profile: RegistrationForm) {
+    const httpOptions = {headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + this.auth.getCredentials() // When creating a profile you should not need to authenticate. Right?
+    })};
+    return this.http.post<Profile>(this.url, profile, httpOptions);
+  }
 
 }
