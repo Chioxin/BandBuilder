93051b68e090cebaa6fd808cf7046d1a685b77db
Jesse <Jesse.mena4@gmail.com>
2019-04-14 10:42:26 -0600

    Update: created and updated the nav-bar components

--- ngBandBuilder/src/app/components/nav-bar/nav-bar.component.ts
+++ ngBandBuilder/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+
+  // Fields
+  newUser: User = new User();
+  selected = '';
+
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
   }
 
+  register() {
+    this.auth.register(this.newUser).subscribe(
+      data => {
+        this.auth.login(this.newUser.username, this.newUser.password).subscribe(
+          dataLogin => {
+              this.selected = 'Yay! it has registered a login with usernaem and password';
+          },
+          err => {
+            console.error(err);
+          }
+        );
+      },
+      err => {
+        console.error(err);
+      }
+    );
+    // this.newUser = new User();
+  }
 }
