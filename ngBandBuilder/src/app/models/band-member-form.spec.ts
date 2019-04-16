d5203338cfe004fc158a6ec3971e4a8b6b72447b
Chioxin <chioxin@gmail.com>
2019-04-16 17:47:35 -0600

    added new ng class of bandMemberForm

---
+++ ngBandBuilder/src/app/models/band-member-form.spec.ts
@@ -0,0 +1,7 @@
+import { BandMemberForm } from './band-member-form';
+
+describe('BandMemberForm', () => {
+  it('should create an instance', () => {
+    expect(new BandMemberForm()).toBeTruthy();
+  });
+});
