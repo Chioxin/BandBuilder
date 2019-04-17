1a491e49abad4c3c6b83188279c2aad143dab230
Jesse <Jesse.mena4@gmail.com>
2019-04-17 15:53:29 -0600

    Style: updated home and registration style

--- ngBandBuilder/src/app/components/home/home.component.ts
+++ ngBandBuilder/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { BandServiceService } from './../../services/band-service.service';
 import { Band } from './../../models/band';
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +14,23 @@ export class HomeComponent implements OnInit {
   bands: Band[] = [];
   searchBy = 'Name';
   searchKeyword = '';
+  images = ['https://cdn.mos.cms.futurecdn.net/wpEgipYD7TJYpuYvbb3mi-320-80.jpg',
+            'https://i.ytimg.com/vi/23pYfDKyfBk/maxresdefault.jpg',
+            'https://www.amwpro.com/wp-content/uploads/band-on-a-stage.jpg'
+          ];
+
 
   // CONSTRUCTOR
 
   constructor(
-    private bandSvc: BandServiceService
-  ) { }
+    private bandSvc: BandServiceService,
+    config: NgbCarouselConfig
+  ) {  // customize default values of carousels used by this component tree
+    config.interval = 5000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = true;
+  }
 
   // INIT
 
