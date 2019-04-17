1045d7f72d6f8bf55a171d5588d4841f31c4b9e2
Jesse <Jesse.mena4@gmail.com>
2019-04-16 18:25:38 -0600

    update: if the profile user is not an admin the button is hidden

--- ngBandBuilder/src/app/components/nav-bar/nav-bar.component.ts
+++ ngBandBuilder/src/app/components/nav-bar/nav-bar.component.ts
@@ -7,6 +7,7 @@ import { FormGroup, NgForm } from '@angular/forms';
 import { logging } from 'protractor';
 import { userInfo } from 'os';
 import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -21,13 +22,17 @@ export class NavBarComponent implements OnInit {
   newUser: User = new User();
   isLoggedIn = false;
   profile: Profile = new Profile();
+  adminRole = false;
   selected = '';
 
   constructor(private auth: AuthService,
-    private router: Router) { }
+    private router: Router,
+    private profileSrv: ProfileService) { }
 
   ngOnInit() {
     this.isLoggedIn = this.auth.checkLogin();
+    this.loadProfile();
+    this.isAdmin();
   }
 
   register() {
@@ -55,6 +60,8 @@ export class NavBarComponent implements OnInit {
         this.selected = 'Logged in!!!';
         this.isLoggedIn = true;
         this.newUser = new User();
+        this.loadProfile();
+        this.isAdmin();
       },
       err => {
         console.error(err);
@@ -66,6 +73,30 @@ export class NavBarComponent implements OnInit {
     this.isLoggedIn = false;
     this.selected = null;
     // navForm.reset();
+  }
+  loadProfile() {
+    const username = this.auth.getUsername();
+    // console.log(username);
+    this.profileSrv.showProfileByUsername(username).subscribe(
+      data => {
+        this.profile = data;
+      },
+      err => {
+        console.error('AdminComponent.loadProfile(): Error');
+        console.error(err);
+      }
+    );
+  }
+  isAdmin() {
+    // this.profile.user.username = this.auth.getUsername();
+    console.log(this.newUser.username);
+
+    if (this.profile.user.role === 'admin') {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
   admin() {
 
